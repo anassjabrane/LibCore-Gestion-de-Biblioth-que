5bddcc26c9c6c1ec9b1c1f502c32733afb725fc8
Anass Jabrane <jabraneanass12@gmail.com>
2026-05-07 10:23:17 +0100

    implémentation de l'ajout de livres et validation terminal

--- LibCore/mainAdmin.php
+++ LibCore/mainAdmin.php
@@ -5,3 +5,17 @@ require_once "src/Services/Database.php";
 $pdo = Database::connect();
 
 echo "Connexion OK ✅";
+
+
+// test us1
+require_once 'src/Entities/User.php';
+require_once 'src/Entities/Book.php';
+require_once 'src/Entities/Librarian.php';
+
+use App\Entities\Book;
+use App\Entities\Librarian;
+
+$admin = new Librarian("Anass Jabrane", "anass@email.com");
+$book = new Book("POO PHP", "Anass", "123-456");
+
+$admin->addBook($book);
--- LibCore/src/Entities/Librarian.php
+++ LibCore/src/Entities/Librarian.php
@@ -4,7 +4,6 @@ namespace App\Entities;
 
 class Librarian extends Users {
     public function addBook( Book $book) :void{
-        echo "✅ [US1] Le livre '" . $book->getTitle() . "' a été ajouté par " . $this->getName();
-
+echo "Le livre '" . $book->getTitle() . "' a été ajouté avec succès par " . $this->getName() . ".\n";
     }
 }
