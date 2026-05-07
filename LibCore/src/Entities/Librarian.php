648799a28d6d7d9066070a718a562413bfd8714a
Anass Jabrane <jabraneanass12@gmail.com>
2026-05-07 12:37:55 +0100

    Ajout des méthodes Librarian pour gérer les livres et membres via Library

--- LibCore/src/Entities/Librarian.php
+++ LibCore/src/Entities/Librarian.php
@@ -1,19 +1,34 @@
 <?php
 
-require_once "User.php";
-require_once __DIR__ . "/../Services/Library.php";
-
-use App\Entities\User;
+namespace App\Entities;
 
 class Librarian extends User {
 
-    private $library;
+    private $Library;
 
     public function __construct($name, $email, $library) {
 
         parent::__construct($name, $email);
 
-        $this->library = $library;
+        $this->Library = $library;
     }
-    
+     public function addBook($book) {
+        $this->Library->addBook($book);
+    }
+
+    public function createMembre($membre) {
+        $this->Library->addMembre($membre);
+    }
+
+    public function displayBooks() {
+        $this->Library->displayBooks();
+    }
+
+    public function deleteBook($bookD) {
+        $this->Library->deleteBook($bookD);
+    }
+
+    public function __toString() {
+        return parent::__toString()." Librarian";
+    }
 }
