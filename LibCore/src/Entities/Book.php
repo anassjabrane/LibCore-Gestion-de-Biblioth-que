2491641d28ed96fef5860a0c541c8b8e8be777c0
Anass Jabrane <jabraneanass12@gmail.com>
2026-05-07 11:30:44 +0100

    Création de la classe Book avec constructeur et attributs

--- LibCore/src/Entities/Book.php
+++ LibCore/src/Entities/Book.php
@@ -3,4 +3,15 @@ class Book{
     public $title;
     public $auteur;
     public $isbn;
-    public $isAvialable;}
+    public $isAvialable;
+    
+    public function __construct( $title ,$auteur ,$isbn , $isAvialable) {
+        $this->title = $title;
+        $this->auteur = $auteur;
+        $this->isbn = $isbn;
+        $this->isAvialable = $isAvialable;
+        
+    }
+    
+    
+    }
